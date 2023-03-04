69dd08b5377a34f780590c9c2295a1aa9e634890
Reda Fadli <fadlireda13@gmail.com>
2023-03-04 17:19:04 +0100

    feat: Navigation bar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    HomeComponent,
+    HomeComponent, NavigationBarComponent,
   ],
   imports: [
     BrowserModule,
