5764ebf4b59f8c7260b0389a1d484856321bd0f5
Reda Fadli <fadlireda13@gmail.com>
2023-04-08 17:11:30 +0200

    08/04

--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListingService } from 'src/services/listing.service';
 import { Listing } from '../listing';
@@ -10,16 +10,35 @@ import { Listing } from '../listing';
 })
 export class ListingsComponent {
 
-  public listings: Listing[] = [];
+  public listings: Listing[] = [
+    { id: 1, title:"First", price:10, city:'Mons', image:"https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp" },
+    { id: 1, title:"Second", price:10, city:'Mons', image:"https://mdbootstrap.com/img/Photos/Slides/img%20(13).webp" },
+    { id: 1, title:"Third", price:10, city:'Mons', image:"https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp" },
+    { id: 1, title:"Fourth", price:10, city:'Mons', image:"https://mdbootstrap.com/img/Photos/Slides/img%20(17).webp" },
+  ];
+  public colsNumber = 0;
+  public innerWidth: any;
 
   constructor(private listingService: ListingService) { }
 
   ngOnInit(): void {
-    this.getListings();
+    // this.getListings();
   }
 
   getListings(): void {
     this.listingService.getListings()
       .subscribe(listings => this.listings = listings);
   }
+
+  // @HostListener('window:resize', ['$event'])
+  // onResize() {
+  //   this.innerWidth = window.innerWidth;
+  //   if (this.innerWidth < 600){
+  //     this.colsNumber = 1;
+  //   } else if (this.innerWidth < 1024) {
+  //     this.colsNumber = 2;
+  //   } else {
+  //     this.colsNumber = 3;
+  //   }
+  // }
 }
